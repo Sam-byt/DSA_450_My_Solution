35760dc3a3abb8b7b1b4218a35d93ab9498ffbc8
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-01-06 16:06:19 +0530

    Inorder Traversal (Iterative)

--- Binary Trees/182.cpp
+++ Binary Trees/182.cpp
@@ -1,39 +1,104 @@
 // { Driver Code Starts
+//Initial Template for C++
+
+
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX_HEIGHT 100000
 
 // Tree Node
 struct Node {
     int data;
     Node* left;
     Node* right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
 };
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
 
 
  // } Driver Code Ends
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child
+//User function Template for C++
+
+/* Tree Node
 struct Node {
     int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-}; */
-
+    Node* left;
+    Node* right;
+};*/
 class Solution {
-  public:
-    // Function to return a list containing the inorder traversal of the tree.
-    vector<int> inOrder(Node* root) {
-        // Your code here
+public:
+    vector<int> inOrder(Node* root)
+    {
+        //code here
         vector<int> v;
         stack<Node *> s;
         
@@ -60,80 +125,30 @@ class Solution {
 
 // { Driver Code Starts.
 
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N') return NULL;
+/* Driver program to test size function*/
 
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;) ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size()) break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
+  
 
 int main() {
+
+   
     int t;
-    string tc;
-    getline(cin, tc);
-    t = stoi(tc);
+    scanf("%d ", &t);
     while (t--) {
-        string s;
+        string s, ch;
         getline(cin, s);
+        
         Node* root = buildTree(s);
 
+        vector<int> ans;
         Solution ob;
-        vector<int> res = ob.inOrder(root);
-        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        ans = ob.inOrder(root) ;
+
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+
         cout << endl;
+     
     }
     return 0;
 }
